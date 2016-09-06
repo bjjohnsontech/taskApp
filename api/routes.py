4b04d7dc94afb3a88bbd7afec52f0ec3403a258f
BJ Johnson <rndmbnjmn@gmail.com>
2016-09-05 21:59:05 -0400

    1. added preliminaries for adding and editing tasks

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template as rendTemp
+from flask import request
 from api import app
 from api.models import database as db, Task, Work
 import json
@@ -30,6 +30,7 @@ def index():
 def get_tasks(id=None, parent=None, top=None):
     total_work = 0 # set total work to 0
     taskL = []
+    # will default to all if id, parent, and top are None
     tasks = Task.select()
     if id:
         tasks = tasks.select().where(Task.id == id)
@@ -50,6 +51,7 @@ def get_tasks(id=None, parent=None, top=None):
                 duration = datetime.datetime.now() - work.start_time
             else:
                 duration = work.end_time - work.start_time
+                # full days = 24 hours * 60 minutes + seconds divisible by 60, leave the remainder
             minutes = duration.days * 24 * 60 + (duration.seconds // 60)
             task['work_complete'] += minutes # add work minutes to this task
             task['work'].append(model_to_dict(work)) # add work row to list
@@ -62,10 +64,55 @@ def get_tasks(id=None, parent=None, top=None):
 @databaseCon
 def get_task(taskID=None):
     if taskID:
-        return json.dumps(get_tasks(id=taskID), default=json_serial)
+        if taskID == 'top':
+            return json.dumps(get_tasks(top=True), default=json_serial)
+        else:
+            return json.dumps(get_tasks(id=taskID), default=json_serial)
     else:
-        return json.dumps(get_tasks(top=True), default=json_serial)
+        data = request.args
+        if 'parent' in data:
+            return json.dumps(get_tasks(parent=data['parent']), default=jsaon_serial)
+        else:
+            return json.dumps(get_tasks(), default=jsaon_serial)
 
-@app.route('/pane_render.html')
-def pane_render():
-    return rendTemp('panes.html')
+@app.route('/task/', methods=['POST'])
+@app.route('/task/<taskID>', methods=['PUT', 'DELETE'])
+@databaseCon
+def do_task(taskID=None):
+    if taskID:
+        if request.method == 'DELETE':
+            task = Task.delete().where(Task.id == taskID)
+            task.execute()
+        elif request.method == 'PUT':
+            data = request.args
+            task = Task.select().where(Task.id == taskID)
+            row = model_to_dict(task)
+            # only change things that need changed
+            if 'name' in data and data['name'] != row['name']:
+                task.name = data['name']
+            if 'description' in data and data['description'] != row['description']:
+                task.description = data['description']
+            if 'parent' in data and data['parent'] != row['parent']['id']:
+                task.parent = data['parent']
+            if 'completed' in data and data['completed'] != row['completed']:
+                task.completed = data['completed']
+            if 'block' in data and data['block'] != row['block']['id']:
+                task.block = data['block']
+            if 'label' in data and data['label'] != row['label']:
+                task.label = data['label']
+            if 'assigned' in data and data['assigned'] != row['assigned']:
+                task.assigned = data['assigned']
+            if 'estimate' in data and data['estimate'] != row['estimate']:
+                task.estimate = data['estimate']
+    else:
+        data = request.args
+        task = Task(
+            name = data.get('name', None),
+            description = data.get('description', None),
+            parent = data.get('parent', None),
+            completed = data.get('completed', None),
+            block = data.get('block', None),
+            label = data.get('label', None),
+            assigned = data.get('assigned', None),
+            estimate = data.get('estimate ', None)
+        )
--- js/tasks.js
+++ js/tasks.js
@@ -4,7 +4,7 @@
     .controller('taskAppController', ['$scope','$http', '$uibModal',
         function($scope, $http, $uibModal) {
             var getTasks = function(){
-                $http({url: '/taskApp/api/task/',
+                $http({url: '/taskApp/api/task/top',
                       method: 'GET'})
                 .then(function taskSuccess(response){
                         $scope.tasks = response.data.tasks;
@@ -15,10 +15,10 @@
             }
             getTasks();
         
-            $scope.newTask = function (id, name, kind) {
+            $scope.task = function(id, name, kind) {
                 id = id || null;
                 name = name || null;
-                kind = kind || 'Add';
+                kind = kind || 'add';
                 var modalInstance = $uibModal.open({
                     animation: true,
                     ariaLabelledBy: 'modal-title',
@@ -28,15 +28,16 @@
                     controllerAs: '$ctrl',
                     size: 'lg',
                     resolve: {
-                      items: function () {
-                        return [kind];
-                      },
-                      name: name,
-                      id: id,
+                      item: function () {
+                        return {kind: kind,
+                                id: id,
+                                name: name
+                                };
+                      }
                     }
                 });
-                modalInstance.result.then(function (selectedItem) {
-                    var e = selectedItem;
+                modalInstance.result.then(function (item) {
+                    var e = item;
                 }, function () {
                     console.log('canceled');
                 });
@@ -44,11 +45,26 @@
         }
         
     ])
-    .controller('ModalInstanceCtrl', function ($uibModalInstance, items, name, id) {
+    .controller('ModalInstanceCtrl',
+    function ($uibModalInstance, item) {
+        var $http = angular.injector(["ng"]).get("$http");
         var $ctrl = this;
-        $ctrl.kind = items[0];
-        $ctrl.id = id;
-        $ctrl.name = name;
+        angular.forEach(item, function(value, key) {
+            this[key] = value;
+        }, $ctrl);
+        console.log($ctrl);
+        $http({url: '/taskApp/api/task/' + $ctrl.id,
+            method: 'GET'})
+        .then(function taskSuccess(response){
+            console.log(response.data);
+            angular.forEach(response.data.tasks[0], function(value, key) {
+                this[key] = value;
+            }, $ctrl);
+            $ctrl.block = $ctrl.block.id || ''
+            $ctrl.estimate = $ctrl.estimate / 60 + 'h';
+        }, function taskFail(response){
+            console.log(response);
+        });
         
         $ctrl.title;
         $ctrl.$onInit = function () {
